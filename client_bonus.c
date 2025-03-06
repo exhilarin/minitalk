be2a63ee136aa5cd7c9f05991abb7afcc367c4ff
Exhilarin <ilyasguney105@gmail.com>
2025-03-07 00:48:09 +0300

    client_bonus.c

--- client_bonus.c
+++ client_bonus.c
@@ -56,7 +56,7 @@ int main(int ac, char *av[])
 		server_pid = ft_atoi(av[1]);
 		if (kill(server_pid, 0) == -1 || server_pid > 4194304
 			|| server_pid == -1)
-			return(ft_printf("Invalid PID\n"), 0);
+			return(ft_printf("Invalid PID\n"), 1);
 		str = av[2];
 		signal(SIGUSR1, signal_handler);
 		bit_converter(server_pid, str);
